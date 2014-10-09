87715b2e49561d8ccd6f7ce78779f14336f455a1
Steven Jack <stevenmajack@gmail.com>
2014-10-09 17:26:29 +0100

    Updates data mapper to handle response better

--- spec/data_mapper_spec.rb
+++ spec/data_mapper_spec.rb
@@ -2,20 +2,41 @@ require "spec_helper"
 
 describe FooMapper do
   let (:api_host) { 'http://www.test-api.com' }
-  let (:context) { { :foo => :bar } }
   let (:connection) { instance_double(Faraday::Connection, :get => nil) }
+  let (:context) do
+    {
+      :foo => :bar
+    }
+  end
 
-  subject { FooMapper.new(api_host, context, connection) }
+  subject { described_class.new(api_host, context, connection) }
 
   describe "#data" do
-    let (:expected_data) { { :some => :data } }
+    let (:expected_raw_data) do
+      {
+        :some => 'data'
+      }
+    end
+    let (:expected_data) { JSON::generate(expected_raw_data, :symbolize_names => true) }
+    let (:expected_response) { instance_double(Faraday::Response, :body => expected_data, :status => 200) }
 
     context "with a valid endpoint" do
       before(:each) do
-        allow(connection).to receive(:get).with("/some/test/endpoint/#{context.values[0]}").and_return(expected_data)
+        allow(connection).to receive(:get).with("/some/test/endpoint/#{context.values[0]}").and_return(expected_response)
       end
 
-      specify { expect(subject.data).to eq expected_data }
+      specify { expect(subject.data).to eq expected_raw_data }
+    end
+
+    context "invalid hostname" do
+      let (:expected_exception) { Alephant::Publisher::Request::ConnectionFailed }
+      let (:faraday_exception) { Faraday::ConnectionFailed.new(StandardError) }
+
+      before(:each) do
+        allow(connection).to receive(:get).and_raise(faraday_exception)
+      end
+
+      specify { expect{ subject.data }.to raise_error expected_exception }
     end
   end
 end
