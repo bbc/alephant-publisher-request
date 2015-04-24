20d309086b2cd0dd42e0983b6c6c1181e3e4c556
Integralist <mark.mcdx@gmail.com>
2015-04-24 16:27:30 +0100

    Remove obsolete method argument

--- lib/alephant/publisher/request/log_helper.rb
+++ lib/alephant/publisher/request/log_helper.rb
@@ -5,7 +5,7 @@ module Alephant
     module Request
       module LogHelper
         include Logger
-        def log_error_with_metric(e, method, message, metric = nil, metric_count = 1)
+        def log_error_with_metric(e, method, message, metric = nil)
           logger.error "Publisher::Request::#{method}: '#{e.class}(#{e.message})' exception raised for: #{message}"
           logger.metric metric unless metric.nil?
         end
